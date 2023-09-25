3d80c539b2d25f3c86ab9a9d6ed24dc6cf392c00
Paul Wögerer <paul.woegerer@oracle.com>
2023-09-25 14:43:05 +0200

    Remove obsolete agent-based image build code.
    (cherry picked from commit 8ce7298bd6e3a6a21006063c0f8482e80fa4dfb8)

--- substratevm/src/com.oracle.svm.agent/src/com/oracle/svm/agent/NativeImageAgent.java
+++ substratevm/src/com.oracle.svm.agent/src/com/oracle/svm/agent/NativeImageAgent.java
@@ -486,6 +486,9 @@ public final class NativeImageAgent extends JvmtiAgentBase<NativeImageAgentJNIHa
         }
     }
 
+    private static final Pattern propertyBlacklist = Pattern.compile("(java\\..*)|(sun\\..*)|(jvmci\\..*)");
+    private static final Pattern propertyWhitelist = Pattern.compile("(java\\.library\\.path)|(java\\.io\\.tmpdir)");
+
     private static String transformPath(String path) {
         String result = path;
         if (result.contains("{pid}")) {
