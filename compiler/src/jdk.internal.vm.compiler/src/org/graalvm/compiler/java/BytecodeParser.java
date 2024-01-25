5f7f1a4182948552767c091afcd627b52a65972f
Carlo Refice <carlo.refice@oracle.com>
2024-01-25 15:58:36 +0100

    BytecodeParser: Do not pop values from FrameState before resolving types/fields
    If, during handling of an instruction, we come across an unresolved type/field, we generate an unconditional deoptimization. In such cases we should maintain the values that are present on the stack intact.
    However, the `handleUnresolvedX` functions in BytecodeParser encouraged popping values from the stack, even though the obtained values were never used. This PR removes ValueNode parameters from `handleUnresolvedX` functions and reorders some function calls to ensure that no values are popped from a FrameState's stack until we're certain we won't emit a deopt.
    
    (cherry picked from commit f4d00852fa8968decff504bcf1bcf7d24fd2110b)

--- compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/java/BytecodeParser.java
+++ compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/java/BytecodeParser.java
@@ -4702,10 +4702,6 @@ public class BytecodeParser extends CoreProvidersDelegate implements GraphBuilde
         if (typeIsResolved(type)) {
             genNewObjectArray((ResolvedJavaType) type);
         } else {
-            /*
-             * The link time effects of an unresolved bytecode always occur before any runtime
-             * exception checks, meaning there is no need to check if the length is positive.
-             */
             handleUnresolvedNewObjectArray(type);
         }
     }
@@ -4734,10 +4730,6 @@ public class BytecodeParser extends CoreProvidersDelegate implements GraphBuilde
             ValueNode[] dims = new ValueNode[rank];
             genNewMultiArray((ResolvedJavaType) type, rank, dims);
         } else {
-            /*
-             * The link time effects of an unresolved bytecode always occur before any runtime
-             * exception checks, meaning there is no need to check if the dims are positive.
-             */
             handleUnresolvedNewMultiArray(type);
         }
     }
