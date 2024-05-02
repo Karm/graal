e58bb91386190159685429e794fab6663805f2e5
Severin Gehwolf <sgehwolf@redhat.com>
2024-05-02 16:40:50 +0200

    Uniformly set version to 23.1.3.1

--- wasm/mx.wasm/suite.py
+++ wasm/mx.wasm/suite.py
@@ -42,7 +42,7 @@ suite = {
   "mxversion": "6.41.0",
   "name" : "wasm",
   "groupId" : "org.graalvm.wasm",
-  "version" : "23.1.2.1",
+  "version" : "23.1.3.1",
   "versionConflictResolution" : "latest",
   "url" : "http://graalvm.org/",
   "developer" : {
