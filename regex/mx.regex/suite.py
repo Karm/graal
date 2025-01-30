ce0091f71f289c590c9b3340137d0fec82651c3f
Michal Karm Babacek <karm@redhat.com>
2025-01-30 21:18:18 +0100

    Unmark suites and bump version to 23.1.6.1 [skip ci]

--- regex/mx.regex/suite.py
+++ regex/mx.regex/suite.py
@@ -43,7 +43,7 @@ suite = {
 
   "name" : "regex",
 
-  "version" : "23.1.5.1",
+  "version" : "23.1.6.1",
   "release" : False,
   "groupId" : "org.graalvm.regex",
   "url" : "http://www.graalvm.org/",
